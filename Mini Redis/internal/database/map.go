435ac022654f8371751b074a2a65694ce87619ab
Azizbek <webcoderazizbek@gmail.com>
2026-03-09 18:17:46 -0200

    mutex

--- Mini Redis/internal/database/map.go
+++ Mini Redis/internal/database/map.go
@@ -1,6 +1,9 @@
 package database
 
-import "redis-clone/internal/model"
+import (
+	"redis-clone/internal/model"
+	"sync"
+)
 
 
 
@@ -10,6 +13,7 @@ var Memory = make(map[string]string)
 
 type Data struct {
   memory map[string]string
+  mu sync.Mutex
 }
 
 func NewData() *Data{
@@ -21,17 +25,22 @@ func NewData() *Data{
 
 
 func (d *Data) Set(key string, data model.Data) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
 	d.memory[key] = data.Value
 	return  nil
 }
 
 func (d *Data) Get(key string) (model.Data) {
+		d.mu.Lock()
+	defer d.mu.Unlock()
 	 data := d.memory[key]
 	return  model.Data{Value: data}
 }
 
 func (d *Data) Del(key string) error {
+		d.mu.Lock()
+	defer d.mu.Unlock()
 	delete(d.memory, key)
-
 	return  nil
 }
